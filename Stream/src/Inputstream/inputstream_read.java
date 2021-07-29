307b54fc57a274e07bbb3f78e8d60bf34e07d816
UMK <haga5032@naver.com>
2021-07-29 12:44:18 +0900

    inputstream_read

---
+++ Stream/src/Inputstream/inputstream_read.java
@@ -0,0 +1,20 @@
+package Inputstream;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class inputstream_read {
+	public static void main(String[] args)throws Exception{
+		InputStream is = new FileInputStream("C://Temp/test.txt");
+		int readByte;
+		/*while((readByte=is.read()) != -1) {
+			System.out.println(readByte);
+		}*/
+		
+		while(true) {
+			readByte = is.read();
+			if(readByte == -1) break;
+			System.out.println((char)readByte);
+		}
+	}
+}
